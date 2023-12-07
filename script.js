f9115ec100d65729eff31ce7821072fe86ff6ec3
Tony <tonyvar666@gmail.com>
2023-12-07 23:53:37 +0530

    completed

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ function buttonclick(val){
     document.getElementById('result').value+=val
     
 }
+
+function clearDisplay(){
+    document.getElementById('result').value=""
+
+}
+
+function equalButton(){
+    let text=document.getElementById('result').value
+    let result=eval(text)
+    document.getElementById('result').value=result
+}
